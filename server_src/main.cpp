2e5aeb9832bed4756eead0017ac4912aaddcefa9
Gabin <gabinrudigoz@gmail.com>
2025-04-18 14:44:01 +0200

    afa

--- server_src/main.cpp
+++ server_src/main.cpp
@@ -1,14 +1,14 @@
-#include "Client.hpp"
-#include "Error.hpp"
+#include "../shared_include/Server.hpp"
+#include "../shared_include/Error.hpp"
 #include <iostream>
 
-int main(int argc, char* argv[])  // Changez const char* à char*
+int main(int argc, const char* argv[])
 {
     try {
-        ClientModule::Client client(argc, argv);
-        client.run();
+        Server server(argc, argv);
+        server.run();
     } catch (const std::exception& e) {
-        std::cerr << "Client error: " << e.what() << std::endl;
+        std::cerr << "Server error: " << e.what() << std::endl;
         return ERROR;
     }
     return SUCCESS;
